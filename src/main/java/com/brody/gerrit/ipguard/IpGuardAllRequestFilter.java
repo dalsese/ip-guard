4d1181b42de44cc08b5ed12362bdf2c21b665122
dalsese <112844651+dalsese@users.noreply.github.com>
2025-09-25 16:30:51 +0900

    Update IpGuardAllRequestFilter.java

--- src/main/java/com/brody/gerrit/ipguard/IpGuardAllRequestFilter.java
+++ src/main/java/com/brody/gerrit/ipguard/IpGuardAllRequestFilter.java
@@ -4,7 +4,6 @@ import com.google.gerrit.httpd.AllRequestFilter;
 import com.google.inject.Singleton;
 
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -17,13 +16,10 @@ public class IpGuardAllRequestFilter extends AllRequestFilter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    HttpServletRequest req = (request instanceof HttpServletRequest) ? (HttpServletRequest) request : null;
-
     String ip = null;
-    if (req != null) {
-      ip = extractClientIp(req);
+    if (request instanceof HttpServletRequest) {
+      ip = extractClientIp((HttpServletRequest) request);
     }
-
     try {
       ClientIpContext.set(ip);
       chain.doFilter(request, response);
@@ -32,20 +28,9 @@ public class IpGuardAllRequestFilter extends AllRequestFilter {
     }
   }
 
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    // no-op
-  }
-
-  @Override
-  public void destroy() {
-    // no-op
-  }
-
   private static String extractClientIp(HttpServletRequest req) {
     String xff = req.getHeader("X-Forwarded-For");
     String ip = null;
-
     if (xff != null && !xff.isEmpty()) {
       String first = xff.split(",")[0].trim();
       ip = IpMatcher.normalize(first);
@@ -61,3 +46,4 @@ public class IpGuardAllRequestFilter extends AllRequestFilter {
     }
     return ip;
   }
+}
