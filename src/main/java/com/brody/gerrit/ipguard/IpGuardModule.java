6ca50956a61a38ca389e0f2c1f1b21072449212c
dalsese <112844651+dalsese@users.noreply.github.com>
2025-09-26 18:10:48 +0900

    Update IpGuardModule.java

--- src/main/java/com/brody/gerrit/ipguard/IpGuardModule.java
+++ src/main/java/com/brody/gerrit/ipguard/IpGuardModule.java
@@ -1,11 +1,12 @@
 package com.brody.gerrit.ipguard;
 
 import com.google.gerrit.extensions.registration.DynamicSet;
-import com.google.gerrit.server.git.ReceivePackInitializer;
 import com.google.gerrit.server.git.UploadPackInitializer;
+import com.google.gerrit.server.git.ReceivePackInitializer;
 import com.google.inject.AbstractModule;
 
 public class IpGuardModule extends AbstractModule {
+
     @Override
     protected void configure() {
         DynamicSet.bind(binder(), UploadPackInitializer.class).to(IpGuardUploadPackInitializer.class);
